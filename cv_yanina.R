69ff91bde56f273eb9d6ce9db5aa65e21597823a
Yanina Bellini Saibene <yabellini@gmail.com>
2020-05-31 20:21:56 -0300

    Initial commit

---
+++ cv_yanina.R
@@ -0,0 +1,10 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Yanina Bellini Saibene",
+  data_location = "data",
+  pdf_location = "",
+  html_location = "https://yabellini.netlify.app/cv/",
+  source_location = "https://github.com/yabellini/CV"
+)
+
+
+#Mi google sheet: https://docs.google.com/spreadsheets/d/1snuLpUOmsGZO_k4_9NqTx789AbliG_Fhj2leqr7ocIc/edit#gid=1730172225
