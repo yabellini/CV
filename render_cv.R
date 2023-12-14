c27d8504551c21f8263270a2bb0e55d52ad8defe
Yanina <yabellini@gmail.com>
2023-12-14 20:36:24 -0300

    Scripts

--- render_cv.R
+++ render_cv.R
@@ -6,16 +6,16 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
 # Knit the HTML version
-rmarkdown::render("cv.Rmd",
+rmarkdown::render("spanish_cv.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "cv.html")
+                  output_file = "spanish_cv.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("cv.Rmd",
+rmarkdown::render("spanish_cv.rmd",
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_loc)
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "cv.pdf")
+                       output = "spanish_cv.pdf")
